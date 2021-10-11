167d8bd5c5611c20bc72516f2c69a4da76cba72a
ivv1as <maximvasyagin40@gmail.com>
2021-10-11 15:24:00 +0300

    Cp9

---
+++ Cp9.py
@@ -0,0 +1,32 @@
+def qwerty(a,b,c,d):
+    if (a>=0 | b>=0 | c>=0 | d>=0):
+        if ((c-a)>=1 and (d-b)>=1) :print('Да') 
+        else:
+            if ((c-d)>=1 and (d-a)>=1) :print('Да') 
+            else: print('Нет')
+    else: print('Числа должны быть целыми и неотрицательными') 
+print('Введите длину первого прямоугольника') 
+try:
+    a = int(input()) 
+except ValueError:
+    print('Число должно быть целым и неотрицательным')
+    exit() 
+try:
+    print('Введите ширину первого прямоугольника') 
+    b = int(input()) 
+except ValueError: 
+    print('Число должно быть целым и неотрицательным') 
+    exit()  
+try:
+    print('Введите длину второго прямоугольника')
+    c = int(input()) 
+except ValueError:
+    print('Число должно быть целым и неотрицательным') 
+    exit()  
+try:
+    print('Введите длинну второго прямоугольника')
+    d = int(input()) 
+except ValueError:
+    print('Число должно быть целым и неотрицательным') 
+    exit() 
+qwerty(a,b,c,d)
